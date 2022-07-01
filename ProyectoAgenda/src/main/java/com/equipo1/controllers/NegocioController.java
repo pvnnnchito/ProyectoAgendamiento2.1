52c662e21025e6b160c0d72063e897314e8ef455
FranciscoSith1 <f.cidsegovia@gmail.com>
2022-07-01 15:34:22 -0400

    haciendo negocio

---
+++ ProyectoAgenda/src/main/java/com/equipo1/controllers/NegocioController.java
@@ -0,0 +1,26 @@
+package com.equipo1.controllers;
+
+import com.equipo1.models.Negocio;
+import com.equipo1.services.NegocioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/negocio")
+public class NegocioController {
+    @Autowired
+    NegocioService negocioService;
+
+    @RequestMapping("")
+    public String registronegocio(@ModelAttribute("negocio") Negocio negocio){
+        return "registroNegocio.jsp";
+    }
+
+    @RequestMapping("/registro")
+    public String
+
+
+
+}
