ba926f9da2b48244ddbf4f40f68b8aa6754dc1c7
LuisMejiasR <luisme.1993@gmail.com>
2022-07-21 15:48:04 -0400

    Slides, Explicacion configuración y rutas React

--- REACT/agendamigo/src/App.js
+++ REACT/agendamigo/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomeServicioComponent from "./components/HomeServicioComponent";
+import OnboardingComponent from "./components/OnboardingComponent";
+import ExplicacionConfiguracionComponent from "./components/ExplicacionConfiguracionComponent";
+
 
 
 
 const App = () => {
-    return(<>
-        <HomeServicioComponent/>
-    </>);
+    return(
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<OnboardingComponent/>}></Route>
+        <Route path='/Paso0config' element={<ExplicacionConfiguracionComponent/>}></Route>
+        <Route path='/HomeServicio' element={<HomeServicioComponent/>}></Route>
+
+    </Routes>
+    </BrowserRouter>);
 };
 
 export default App;
